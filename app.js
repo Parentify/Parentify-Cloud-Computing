98c092140a0d31ff87ec4182a0975108a822874d
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2023-11-30 22:44:04 +0700

    Fixed port for cloud

--- app.js
+++ app.js
@@ -16,11 +16,13 @@ let bijiBacang = [
     },
 ];
 
-app.get('/biji-bacang', (res) => {
+app.get('/biji-bacang', (req, res) => {
     res.send(bijiBacang);
 });
 
-const PORT = process.env.PORT || 8000;
-app.listen(PORT, () => {
-    console.log('Server is up and listening on ' + PORT);
+const server = app.listen(process.env.PORT || 8080, () => {
+    const host = server.address().address;
+    const port = server.address().port;
+
+    console.log('Server listening at http://${host}:${port}');
 });
