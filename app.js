5cfefb4619ea130488e6b31dc949f21d1ae8a493
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2023-12-01 13:55:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 
 app.use(router);
 
+app.get('/', (req, res) => {
+    res.send('Successfully connected to the server');
+});
+
 const server = app.listen(process.env.PORT || 8080, () => {
     const host = server.address().address;
     const port = server.address().port;
