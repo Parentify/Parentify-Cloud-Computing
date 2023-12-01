41cf8dc12fd3b4a636693a8261f2d9e401fb5f0a
Raditya Lungguk Satya Putra <radityalungguk@gmail.com>
2023-12-01 12:37:21 +0700

    Add files via upload

---
+++ handler.js
@@ -0,0 +1,35 @@
+const bacang = [
+    {
+        "name": "Bacang",
+        "type": "Biji",
+        "information": ["Hamil", "Menyusui", "Pasca Melahirkan", "Bayi"],
+        "status": ["Tidak Direkomendasikan", "Tidak Direkomendasikan", "Tidak Direkomendasikan", "Dilarang"],
+        "nutrition": ["Sodium", "Kalium", "Vitamin A", "Vitamin B12"]
+    }
+];
+
+const bakpauIsi = [
+    {
+        "name": "Bakpau Isi",
+        "type": "Biji",
+        "information": ["Hamil", "Menyusui", "Pasca Melahirkan", "bayi"],
+        "status": ["Direkomendasikan", "Direkomendasikan", "Direkomendasikan", "Direkomendasikan"],
+        "nutrition": ["Karbohidrat", "Kalium", "Sodium", "Protein", "Lemak"]
+    }
+];
+
+const bekatul = [
+    {
+        "name": "Bekatul",
+        "type": "Biji",
+        "information": ["Hamil", "Menyusui", "Pasca Melahirkan", "bayi"],
+        "status": ["Direkomendasikan", "Direkomendasikan", "Direkomendasikan", "Tidak Direkomendasikan"],
+        "nutrition": ["Protein", "Karbohidrat", "Diet Serat", "Thaimin", "Riboflavin"]
+    }
+];
+
+module.exports = {
+    bacang,
+    bakpauIsi,
+    bekatul
+};
