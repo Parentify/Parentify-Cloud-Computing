c5b8b59de7a32a111046ef2de1876e2084e161a0
Faris Munir <120432426+farismnrr@users.noreply.github.com>
2023-12-04 23:34:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -9,8 +9,5 @@ app.get('/', (req, res) => {
 });
 
 const server = app.listen(process.env.PORT || 8080, () => {
-    const host = server.address().address;
-    const port = server.address().port;
-
-    console.log('Server listening at http://${host}:${port}');
+    console.log('Server listening at http://${process.env.PORT}');
 });
